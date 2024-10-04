838134b0e51ac22dec5c507e90234e1c6bb452b2
Najwah12 <13jujuenes@gmail.com>
2024-10-04 09:33:43 +0700

    second commit

--- Pemilihan2Percobaan115.java
+++ Pemilihan2Percobaan115.java
@@ -8,8 +8,7 @@ import java.util.Scanner;
             System.out.print("Masukkan Tahun: ");
             Tahun = input.nextInt();
 
-            if ((Tahun % 4) == 0) {
-                if ((Tahun % 100) != 0)
+            if ((Tahun % 4 == 0 && Tahun % 100 != 0) || (Tahun % 400 == 0)) {
                     System.out.println("Tahun Kabisat");
             }else {
                 System.out.println("Bukan Tahun Kabisat");
